584aa0517b3f6c573932ecaf6008461899fb90b6
Chetan Singh <chetan@Chetans-MacBook-Air.local>
2025-03-31 20:03:51 +0530

    Modifications

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,8 +4,8 @@ import lightningImg from '/lightning-bolt.svg'
 
 function Error({ message }) {
   return (
-    <div className="text-4xl md:text-5xl font-bold md:px-16 grid grid-cols-1 gap-8 md:gap-16 sm:grid-cols-2 justify-center items-center">
-      <img src={lightningImg} alt="thunderbolt" className="w-[800px]" />
+    <div className="text-2xl md:text-5xl font-bold md:px-16 grid grid-cols-1 gap-8 md:gap-16 sm:grid-cols-2 justify-center items-center">
+      <img src={lightningImg} alt="thunderbolt" />
       {message === "Not Found" ? "Could not fetch data." : message}
     </div>
   );
--- src/components/Fallback.jsx
+++ src/components/Fallback.jsx
@@ -5,8 +5,8 @@ import compassImg from "/compass.svg";
 function Fallback() {
   return (
     <div className="grid grid-cols-1 gap-8 md:gap-16 sm:grid-cols-2 justify-between items-center md:px-16">
-      <img className="w-[800px]" src={compassImg} alt="compass" />
-      <p className="text-4xl md:text-5xl font-bold">
+      <img src={compassImg} alt="compass" />
+      <p className="text-2xl md:text-5xl font-bold">
         Search for a city to get weather details.
       </p>
     </div>
